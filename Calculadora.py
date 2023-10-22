d25a71664eedb579b8962c62b2118a7ce8e6b8a0
BrunoKaykiR <brunoribeiropro24@gmail.com>
2023-10-22 20:52:24 -0300

    START COMMIT

---
+++ Calculadora.py
@@ -0,0 +1,14 @@
+####    PROJETO CALCULADORA (FUNÇÕES)    ###############
+#Bruno Ribeiro
+
+def soma(number1, number2):
+    return number1 + number2
+
+def sub(number1, number2):
+    return number1 - number2
+
+def multi(number1, number2):
+    return number1 * number2
+
+def divi(number1, number2):
+    return number1 / number2
---
+++ main.py
@@ -0,0 +1,53 @@
+#### CALCULADORA #####
+
+from Calculadora import soma, sub, multi, divi
+from time import sleep
+
+
+while True:
+    print('\n\n\t\t\t\t\tSeja Bem-Vindo a Calculadora')
+
+    operation = input('Digite o símbolo da operação:\n[+] Soma\n[-] Subtração\n[*] Multiplicação\n[/] Divisão\n[>_] Sair\n').upper().strip()
+
+    match operation:
+        case '+':
+            number1 = float(input('Insira o primeiro número: '))
+            number2 = float(input('Insira o segundo número: '))
+            print(f'O resultado de {number2} + {number2} = {soma(number1, number2):.2f}\n')
+
+        case '-':
+            number1 = float(input('Insira o primeiro número: '))
+            number2 = float(input('Insira o segundo número: '))
+            print(f'O resultado de {number2} - {number2} = {sub(number1, number2):.2f}\n')
+
+        case '*':
+            number1 = float(input('Insira o primeiro número: '))
+            number2 = float(input('Insira o segundo número: '))
+            print(f'O resultado de {number2} - {number2} = {multi(number1, number2):.2f}\n')
+
+        case '/':
+            number1 = float(input('Insira o primeiro número: '))
+            number2 = float(input('Insira o segundo número: '))
+            print(f'O resultado de {number2} - {number2} = {divi(number1, number2):.2f}\n')
+
+        case '>_':
+            print('Finalizando o Sistema...\n')
+            sleep(2)
+            print('SISTEMA FINALIZADO!')
+            break
+
+        case _:
+            print('ERROR! Opção Inválida!')
+
+    stop = input('Deseja realizar outra operação? (Y/N)').upper().strip()
+
+    match stop:
+        case 'Y':
+            print('Reiniciando o sistema, aguarde...')
+            sleep(2)
+
+        case 'N':
+            print('Finalizando o Sistema...\n')
+            sleep(2)
+            print('SISTEMA FINALIZADO!')
+            break
